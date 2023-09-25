04251af71cecbd7c1451e26ca77199b4154e4945
H20Beetle <waterbeetle091@gmail.com>
2023-09-25 00:34:18 -0500

    update

---
+++ basics.ts
@@ -0,0 +1,18 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Functions types, Parameters
+
+//Primitives
+
+let age: number; 
+
+age = 12;
+
+let userName: string;
+
+userName = 'Kyle';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
